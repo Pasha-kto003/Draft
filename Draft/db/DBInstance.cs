2dd2c2545fcfc545149a7c91e31d4ee2ab6e99b9
Паша Луковников <lukovnikov.pasha@gmail.com>
2021-12-07 17:11:43 +1000

    Добавьте файлы проекта.

---
+++ Draft/db/DBInstance.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Draft.db
+{
+    public class DBInstance
+    {
+        static test_DBEntities connection;
+        static object objectLock = new object();
+        public static test_DBEntities Get()
+        {
+            lock (objectLock)
+            {
+                if (connection == null)
+                    connection = new test_DBEntities();
+                return connection;
+            }
+        }
+    }
+}
